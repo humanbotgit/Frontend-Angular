2a63dd0d714903285a123b7a47fc89bfe9274e7c
humanbotgit <jarancibiasedano@gmail.com>
2023-11-10 10:09:34 -0500

    boton enviar arreglado

--- Sistema-Reserva/src/app/forms/new-reserva/new-reserva.component.ts
+++ Sistema-Reserva/src/app/forms/new-reserva/new-reserva.component.ts
@@ -38,12 +38,14 @@ export class NewReservaComponent implements OnInit {
     this.fechaDeRegistro = new Date();
     this.dateAdapter.setLocale('es');
     this.minDate = new Date();
+    //Obtienene el dni del docente
     this.dniDocente = this.profesorService.getDNIDocente() || 'ValorPredeterminado';
+    //carga las asignaturas del docente
     this.loadAsignaturas();
   }
 
   ngOnInit() {}
-
+  //carga las asignaturas
   loadAsignaturas() {
     if (this.dniDocente) {
       this.crearReservaService.getAsignaturas(this.dniDocente).subscribe((data) => {
@@ -51,13 +53,13 @@ export class NewReservaComponent implements OnInit {
       });
     }
   }
-
+//carga los laboratorios al haber seleccionado la asignatura
   onAsignaturaChange() {
     if (this.selectedAsignatura && this.dniDocente) {
       this.loadLaboratorios();
     }
   }
-
+  //carga los laboratorios
   loadLaboratorios() {
     if (this.selectedAsignatura && this.dniDocente) {
       this.crearReservaService.getLaboratorios(this.selectedAsignatura, this.dniDocente).subscribe((data) => {
@@ -70,7 +72,13 @@ export class NewReservaComponent implements OnInit {
       console.log('Valores insuficientes para cargar laboratorios.');
     }
   }
-
+  //cuando selecciones el laboratorio cargara la cantidad de licencias
+  onLaboratorioChange() {
+    if (this.selectedLaboratorio && this.dniDocente) {
+      this.loadCantidadLicencia();
+    }
+  }
+  //carga cantidad de licencias
   loadCantidadLicencia() {
     if (this.selectedLaboratorio) {
       this.crearReservaService.getCantidadLicencias(this.selectedLaboratorio).subscribe(
@@ -86,51 +94,34 @@ export class NewReservaComponent implements OnInit {
       console.log('Esperando la selección de un laboratorio para cargar las licencias.');
     }
   }
-
-  onLaboratorioChange() {
-    if (this.selectedLaboratorio && this.dniDocente) {
-      this.loadCantidadLicencia();
-      this.updateButtonState();
-    }
+  getCantidadLicenciasSolicitadas(): number {
+    return parseInt(this.cantidadLicencias, 10);
   }
-
+  //compara las licencias
   onInputChange(event: Event) {
     const input = event.target as HTMLInputElement;
     const inputValue = input.value;
     const numericValue = parseInt(inputValue, 10);
-
     if (isNaN(numericValue)) {
       this.errorMessage = 'Por favor, introduce un número válido.';
       this.showError = true;
+      this.isButtonDisabled=true;
     } else {
       if (numericValue > this.availableLicenses) {
         this.errorMessage = 'No hay suficientes licencias disponibles';
         this.showError = true;
+        this.isButtonDisabled=true;
       } else {
         this.errorMessage = '';
         this.showError = false;
         this.cantidadLicencias = numericValue.toString();
+        this.isButtonDisabled=false;
       }
     }
-    this.updateButtonState();
-
   }
-
-  getCantidadLicenciasSolicitadas(): number {
-    return parseInt(this.cantidadLicencias, 10);
-  }
-
   cantidadLicenciasValida(): boolean {
     const cantidadSolicitada = parseInt(this.cantidadLicencias, 10);
     return cantidadSolicitada <= this.availableLicenses;
-  }
-  
-
-  getButtonClass(): string {
-    return this.selectedLaboratorio && this.cantidadLicenciasValida() ? 'btn-primary' : 'btn-secondary';
-  }
-  updateButtonState(): void {
-    this.isButtonDisabled = !(this.selectedLaboratorio && this.cantidadLicenciasValida());
   }
   onSubmit(form: NgForm) {
     if (form.valid) {
